4d5c4e4a5881b7efbccd94134ff3493c53897d74
Glauco <83071902+glaucodesouza@users.noreply.github.com>
2025-02-14 10:15:23 -0300

    Update service.js

--- how-to-call-api-with-destination-from-your-backend-app-v2/srv/service.js
+++ how-to-call-api-with-destination-from-your-backend-app-v2/srv/service.js
@@ -1,6 +1,11 @@
   const destination = await cds.connect.to('MY_DESTINATION');
 
-  this.on('process', async (req) => {
+// this function here, is called from who call my backend service.
+// the url they call me is the same <url as you have deployed>+<yourService Path you put in service.cds>
+//example of url with no path declared (CAP standard path): https://your-customer-dev-myDeployedAppName-srv.cfapps.us10.hana.ondemand.com/odata/v4/myServiceName/mYProcessFunction
+//example of url called when deployed app (with your path declared): https://your-customer-dev-myDeployedAppName-srv.cfapps.us10.hana.ondemand.com/service/myServiceName/mYProcessFunction
+//example of url while in local test: https://port4004-workspaces-ws-zz999.us10.applicationstudio.cloud.sap/odata/v4/myServiceName/mYProcessFunction
+  this.on('mYProcessFunction', async (req) => {
 
     try {           
       const response = await destination.send({
